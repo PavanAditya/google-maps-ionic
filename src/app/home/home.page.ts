1183ccdc4cbcf96144d1c5846c4314bdb01c0c40
Pavan Aditya M S <msp.aditya99@gmail.com>
2019-06-02 22:00:23 +0530

    add marker option added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
+declare var google;
 
 @Component({
   selector: 'app-home',
@@ -6,7 +10,50 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
 
-  constructor() {}
+  constructor(
+    private navController: NavController,
+    private geolocation: Geolocation
+  ) { }
+
+  ionicViewDidLoad() {
+    this.loadMap();
+  }
+
+  public loadMap(): void {
+    this.geolocation.getCurrentPosition().then((position) => {
+      const latLng = new google.maps.LatLang(position.coords.latitude, position.coords.longitude);
+      const mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+  public addMarker(): void {
+
+    const marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+    });
+    const content = `<h4>Information!</h4>`;
+    this.addInfoWindow(marker, content);
+  }
+
+  public addInfoWindow(marker, content): void {
+    const infoWindow = new google.maps.InfoWindow({
+      content
+    });
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    });
+  }
 
 }
